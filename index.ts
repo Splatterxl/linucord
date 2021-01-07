48e7f8a19c7b4d3d42e1e9fc882f6d0de5b50cc2
nearlySplat <splatterxl@outlook.ie>
2021-01-07 17:19:14 +0000

    Update index.ts

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ export const client: Client = new Client(),
   prefixes = [
     "sudo "
   ],
-  cmds = new Collection<string, ((Message, string[]) => string)>();
+  cmds = new Collection<string, ({ run: ((Message, string[]) => string) })>();
 
 cmds.set("sys", (m, a) => (a[1] == "ping") ? `Pong! WebSocket Latency: ${client.ws.ping}ms` : "sys: unknown system command")
 
