ba9900a855a11f1bfe1c6aea4428311405484de9
nearlySplat <splatterxl@outlook.ie>
2021-01-08 13:43:49 +0000

    Update sudo.ts

--- commands/sudo.ts
+++ commands/sudo.ts
@@ -2,15 +2,15 @@ import { Message } from "discord.js";
 import { CommandOutput } from "../structures/text";
 
 export const run = async (msg: Message, a: string[]) => {
-  const prompt = await msg.channel.send(new CommandOutput(msg, `[sudo]: password for ${msg.author.username.toLowerCase().replace(/( |_)/g, "")}: \n\n\nThis prompt will timeout in 15 seconds.`));
+  const prompt = await msg.channel.send(CommandOutput(msg, `[sudo]: password for ${msg.author.username.toLowerCase().replace(/( |_)/g, "")}: \n\n\nThis prompt will timeout in 15 seconds.`));
   const passwdCollector = msg.channel.createMessageCollector(mc => mc.author.id == msg.author.id, { time: 15000 });
   const passwd = { correct: `${msg.guild.id}~${msg.author.id}`, has: false };
   passwdCollector.on("collect", m => {
     if (passwd.correct == m.content) { 
-      prompt.edit(new CommandOutput(msg, prompt.content.replace("This prompt will timeout in 15 seconds.", "Correct password input.")));
+      prompt.edit(CommandOutput(msg, prompt.content.replace("This prompt will timeout in 15 seconds.", "Correct password input.")));
       passwdCollector.end();
     } else {
-      prompt.edit(new CommandOutput(msg, "Wrong password. Try again."))
+      prompt.edit(CommandOutput(msg, "Wrong password. Try again."))
       passwdCollector.end()
     }
   })
