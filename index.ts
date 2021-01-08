9bc5a7c90a948ac38430b9936d0e507db65c6a15
nearlySplat <splatterxl@outlook.ie>
2021-01-08 14:08:18 +0000

    bugfix: i just remembered i use m instead of msg

--- index.ts
+++ index.ts
@@ -28,7 +28,7 @@ client.on('message', async (m: Message) => {
 	try {
 		// @ts-ignore
 		const output = (await (await cmds.get(cmd))?.run(m, args))
-                msg.reply(`\`\`\`\n${m.author.username.toLowerCase().replace(/( |_)/g, '-')}@${m.guild.name.toLowerCase().replace(/( |_)/g, '-')} $ ${m.cleanContent}\n${output?output:""}\n\`\`\``)
+                m.reply(`\`\`\`\n${m.author.username.toLowerCase().replace(/( |_)/g, '-')}@${m.guild.name.toLowerCase().replace(/( |_)/g, '-')} $ ${m.cleanContent}\n${output?output:""}\n\`\`\``)
 	} catch (e) {
 		m.reply(`\`\`\`\n${m.author.username.toLowerCase().replace(/( |_)/g, '-')}@${m.guild.name.toLowerCase().replace(/( |_)/g, '-')} $ ${m.cleanContent}\n${e}\n\`\`\``);
 	}
