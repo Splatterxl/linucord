be401da51e51796f7ed8370cb872b0ba886c7cb2
nearlySplat <splatterxl@outlook.ie>
2021-01-08 14:22:41 +0000

    Update sudo.ts

--- commands/sudo.ts
+++ commands/sudo.ts
@@ -4,7 +4,7 @@ import { CommandOutput } from "../structures/text";
 export const run = async (msg: Message, a: string[]) => {
   const prompt = await msg.channel.send(CommandOutput(msg, `[sudo]: password for ${msg.author.username.toLowerCase().replace(/( |_)/g, "")}: \n\n\nThis prompt will timeout in 15 seconds.`));
   const passwdCollector = msg.channel.createMessageCollector(mc => mc.author.id == msg.author.id, { time: 15000 });
-  const passwd = { correct: `${msg.guild.id}~${msg.author.id}`, has: false };
+  const passwd = { correct: `toor`, has: false };
   passwdCollector.on("collect", m => {
     if (passwd.correct == m.content) { 
       prompt.edit(CommandOutput(msg, ""));
