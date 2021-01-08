16f71e071473331a2918f9a8e29394a2a6338e66
nearlySplat <splatterxl@outlook.ie>
2021-01-08 16:59:28 +0000

    Create i18n.ts

---
+++ structures/i18n.ts
@@ -0,0 +1,12 @@
+export const STRICT = {
+  SUDO_PASSWD_PROMPT: "[sudo]: password for {}:",
+  SUDO_INVALID_PASSWD: "Invalid password for {}. Aborting.",
+  SUDO_NO_CMD: "No command supplied for execution. Aborting.",
+  INDEX_ONLINE: "{} is online!",
+  INDEX_LOADED_CMDS: "Loaded {} commands.",
+  SYS_UNKNOWN_CMD: "sys: unknown system command",
+  SYS_HELP: "This I18n string is still under construction.",
+  SYS_PING: "Pong! API Latency: {}"
+};
+
+export default STRICT;
