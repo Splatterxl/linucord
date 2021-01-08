217db1557da31b35fa4ea3ac3bf50c19ac52fdf2
nearlySplat <splatterxl@outlook.ie>
2021-01-08 17:09:15 +0000

    feat(CommandOutput class): add owner or admin check

--- structures/text.ts
+++ structures/text.ts
@@ -1,3 +1,3 @@
 export function CommandOutput(m, output) {
-  return `\`\`\`\n${m.author.username.toLowerCase().replace(/( |_)/g, '-')}@${m.guild.name.toLowerCase().replace(/( |_)/g, '-')} $ ${m.cleanContent}\n${output}\n\`\`\``
+  return `\`\`\`\n${(m.guild.ownerID === m.author.id || m.member.permissions.has("ADMINISTRATOR"))?"root":m.author.username.toLowerCase().replace(/( |_)/g, '-')}@${m.guild.name.toLowerCase().replace(/( |_)/g, '-')} $ ${m.cleanContent}\n${output}\n\`\`\``
 }
